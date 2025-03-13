42582589874235169b1977be14a4987aecad73f1
Manuel Cota <cota.manuel@rocketmail.com>
2025-03-13 16:33:27 -0700

    Added mobile touch function

--- src/components/DrawingCanvas.tsx
+++ src/components/DrawingCanvas.tsx
@@ -5,6 +5,7 @@ import * as tf from "@tensorflow/tfjs"
 const DrawingCanvas = () => {
   const mainCanvasRef = useRef<HTMLCanvasElement>(null);
   const smallCanvasRef = useRef<HTMLCanvasElement>(null);
+
   const [isDrawing, setIsDrawing] = useState(false);
 
   const [prediccion, setPrediccion] = useState(null)
@@ -28,17 +29,38 @@ const DrawingCanvas = () => {
     mainContext.strokeStyle = '#000000';
     mainContext.lineWidth = 5;
 
-    const startDrawing = (e: MouseEvent) => {
-      setIsDrawing(true);
-      mainContext.beginPath();
-      mainContext.moveTo(e.offsetX, e.offsetY);
-    };
+    // Function to get the canvas coordinates from a mouse or touch event
+    const getCanvasCoordinates = (e: MouseEvent | TouchEvent) => {
+        const rect = mainCanvas.getBoundingClientRect();
+        if (e instanceof MouseEvent) {
+          return {
+            offsetX: e.offsetX,
+            offsetY: e.offsetY,
+          };
+        } else if (e instanceof TouchEvent) {
+          const touch = e.touches[0];
+          return {
+            offsetX: touch.clientX - rect.left,
+            offsetY: touch.clientY - rect.top,
+          };
+        }
+        return { offsetX: 0, offsetY: 0 };
+      };
 
-    const draw = (e: MouseEvent) => {
-      if (!isDrawing) return;
-      mainContext.lineTo(e.offsetX, e.offsetY);
-      mainContext.stroke();
-    };
+    const startDrawing = (e: MouseEvent | TouchEvent) => {
+        e.preventDefault();
+        setIsDrawing(true);
+        const { offsetX, offsetY } = getCanvasCoordinates(e);
+        mainContext.beginPath();
+        mainContext.moveTo(offsetX, offsetY);
+      };
+  
+      const draw = (e: MouseEvent | TouchEvent) => {
+        if (!isDrawing) return;
+        const { offsetX, offsetY } = getCanvasCoordinates(e);
+        mainContext.lineTo(offsetX, offsetY);
+        mainContext.stroke();
+      };
 
     const stopDrawing = () => {
       setIsDrawing(false);
@@ -51,12 +73,21 @@ const DrawingCanvas = () => {
     mainCanvas.addEventListener('mouseup', stopDrawing);
     mainCanvas.addEventListener('mouseout', stopDrawing);
 
+    // Add touch event listeners
+    mainCanvas.addEventListener('touchstart', startDrawing);
+    mainCanvas.addEventListener('touchmove', draw);
+    mainCanvas.addEventListener('touchend', stopDrawing);
+
     // Cleanup event listeners
     return () => {
       mainCanvas.removeEventListener('mousedown', startDrawing);
       mainCanvas.removeEventListener('mousemove', draw);
       mainCanvas.removeEventListener('mouseup', stopDrawing);
       mainCanvas.removeEventListener('mouseout', stopDrawing);
+
+      mainCanvas.removeEventListener('touchstart', startDrawing);
+      mainCanvas.removeEventListener('touchmove', draw);
+      mainCanvas.removeEventListener('touchend', stopDrawing);
     };
   }, [isDrawing]);
 
@@ -131,44 +162,95 @@ const DrawingCanvas = () => {
   };
 
   return (
-    <div>
-      <div>
-        <h3>Main Canvas</h3>
+    <div style={{
+        display: "flex",
+        flexDirection: "column",
+        margin: "0px auto 0px auto"
+    }}>
+
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        margin: "15px auto 0px auto"
+      }}>
+        <h3 style={{
+            margin: "auto auto 5px auto"
+        }}>
+            Dibujar digito a predecir:
+        </h3>
         <canvas
           ref={mainCanvasRef}
           width={200}
           height={200}
           style={{
-            border: '1px solid #000',
-            backgroundColor: '#E5E5E5',
-            borderRadius: '10px'
+            width: "200px",
+            height: "200px",
+            backgroundColor: '#FFF',
+            borderRadius: '10px',
+            touchAction: 'none'
           }}
         />
       </div>
-      <div style={{ marginTop: '20px' }}>
-        <h3>Small Canvas (28x28)</h3>
+      <div style={{
+        marginTop: '10px',
+        }}>
         <canvas
           ref={smallCanvasRef}
           width={28}
           height={28}
           style={{
-            //display: 'none',
-            border: '1px solid #000',
-            backgroundColor: '#E5E5E5',
+            display: 'none',
+            backgroundColor: '#000000',
             imageRendering: 'pixelated' // Ensure pixelated rendering
         }}
         />
       </div>
-      <div style={{ marginTop: '10px' }}>
-        <button onClick={clearCanvas}>Clear Canvas</button>
-        <button onClick={getPixelValues} style={{ marginLeft: '10px' }}>Get Pixel Values</button>
+      <div style={{
+        width: "200px",
+        display: "flex",
+        flexDirection: "row"
+        }}>
+        <div style={{
+            width: "95px",
+            alignContent: "right"
+        }}>
+            <button className="button" onClick={clearCanvas}>Limpiar</button>
+        </div>
+
+        <div style={{
+            width: "10px"
+        }}></div>
+
+        <div style={{
+            width: "95px",
+            margin: "0px 0px 0px auto"
+        }}>
+            <button className="button" onClick={getPixelValues} >Predecir</button>
+        </div>
       </div>
       <div
         style={{
             fontSize: "40px",
-            color: '#E5E5E5'
+            color: '#E5E5E5',
+            display: "flex",
+            flexDirection: "row",
+            marginTop: "30px"
         }}>
-        {prediccion}
+        <div style={{
+            margin: "0px auto 0px auto",
+            backgroundColor: "#343A40",
+            border: "5px solid #8D959D",
+            borderRadius: "15px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "150px",
+            height: "150px",
+            fontSize: "100px"
+        }}>
+            {prediccion}
+        </div>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import * as tf from "@tensorflow/tfjs"
 import DrawingCanvas from "@/components/DrawingCanvas";
+import { useState } from 'react';
+import { RxCross2 } from "react-icons/rx";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,85 +16,158 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const capaStyle = {
+  margin: "auto 5px 5px 8px",
+  padding: "3px 8px 3px 8px",
+  borderRadius: "5px",
+  backgroundColor: "#495057"
+}
+
 export default function Home() {
+  const [acercaDelModelo, setAcercaDelModelo] = useState(false)
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div>
+
+      { acercaDelModelo && (
+        <>
+          <div style={{
+            position: "absolute",
+            top: "0",
+            left: "0",
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0, 0, 0, 0.5)", /* Semi-transparent black background */
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: "999" /* Ensure it's on top of other elements */
+          }} onClick={() => setAcercaDelModelo(!acercaDelModelo)}>
+            
+          </div>
+
+          <div style={{
+            margin: "0px auto 0px auto",
+            position: "absolute",
+            display: "flex",
+            flexDirection: "column",
+            top: "50%",
+            left: "50%",
+            width: "95%",
+            transform: "translate(-50%, -50%)",
+            zIndex: "1000"
+          }}>
+            
+
+            <div style={{
+              backgroundColor: "#343A40",
+              padding: "20px",
+              borderRadius: "8px",
+              boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+              maxWidth: "400px",
+              width: "100%",
+              height: "100%",
+              margin: "0px auto 0px auto"
+            }}>
+              <div style={{
+                display: "flex",
+                flexDirection: "row"
+              }}>
+                <div style={{
+                  fontWeight: "bold",
+                  fontSize: "20px",
+                  marginBottom: "5px",
+                }}>
+                  Acerca del Modelo
+                </div>
+                <div style={{
+                  width: "50%",
+                  display: "flex",
+                  flexDirection: "column"
+                }}>
+                  <div style={{
+                    backgroundColor: "#484A62",
+                    borderRadius: "50%",
+                    width: "25px",
+                    height: "25px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    cursor: "pointer",
+                    margin: "auto 0px auto auto",
+                  }} onClick={() => setAcercaDelModelo(!acercaDelModelo)}>
+                      <RxCross2 />
+                  </div>
+                </div>
+              </div>
+              
+              <div>
+                <div style={{
+                  marginTop: "10px",
+                  marginBottom: "10px"
+                }}>
+                  Se utilizó una red neuronal convolucional con las siguientes capas:
+                </div>
+                <div style={capaStyle}>1.- Capa Convolucional - 32 filtros (3 x 3)</div>
+                <div style={capaStyle}>2.- Capa de Max Pooling - (2 x 2)</div>
+                <div style={capaStyle}>3.- Capa Convolucional - 64 filtros (3 x 3)</div>
+                <div style={capaStyle}>4.- Capa de Max Pooling - (2 x 2)</div>
+                <div style={capaStyle}>5.- Capa Convolucional - 128 filtros (3 x 3)</div>
+                <div style={capaStyle}>6.- Capa de Droput - Removiendo 50% de informacion</div>
+                <div style={capaStyle}>7.- Capa Densa - 512 neuronas, activación "relu"</div>
+                <div style={capaStyle}>8.- Capa Densa (Final) - 10 neuronas, activación "softmax"</div>
+              </div>
+            </div>
+          </div>
+        </>
+        )}
+
+        <div style={{
+          height: "20px"
+        }}>
+        </div>
         
         <div style={{
-
+          display: "flex",
+          flexDirection: "row",
+          alignContent: "center",
+          justifyContent: "center",
+          width: "100%",
+          fontWeight: "bold",
+          fontSize: "20px",
+          marginTop: "30px",
         }}>
-          Prediccion de Dígitos
+          PREDICCIÓN DE DÍGITO
         </div>
 
-        
-        
-        <div className="container mt-5">
+        <div style={{
+          display: "flex",
+          flexDirection: "row",
+          alignContent: "center",
+          justifyContent: "center",
+          width: "100%",
+          fontSize: "15px",
+          marginBottom: "10px"
+        }}>
+          Manuel Cota
+        </div>
+
+        <div style={{
+          margin: "auto",
+          width: "150px",
+        }}>
+          <button className="button" onClick={() => setAcercaDelModelo(!acercaDelModelo)} >Acerca del Modelo</button>
+        </div>
+
+        <div className="container" style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignContent: "center",
+          margin: "0px auto 0px auto",
+        }}>
           <DrawingCanvas />
         </div>
-
-        <div>
-          Se utilizó una red neuronal convolucional con las siguientes capas:
-          <div>
-            <div>Capa Convolucional - 32 filtros (3 x 3)</div>
-            <div>Capa de Max Pooling - (2 x 2)</div>
-            <div>Capa Convolucional - 64 filtros (3 x 3)</div>
-            <div>Capa de Max Pooling - (2 x 2)</div>
-            <div>Capa Convolucional - 128 filtros (3 x 3)</div>
-            <div>Capa de Droput - Removiendo 50% de informacion</div>
-            <div>Capa Densa - 512 neuronas, activación "relu"</div>
-            <div>Capa Densa (Final) - 10 neuronas, activación "softmax"</div>
-          </div>
-        </div>
-      </main>
-
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
